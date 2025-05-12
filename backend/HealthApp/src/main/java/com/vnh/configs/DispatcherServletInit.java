ba8652b884a6b5b4d315e2b85f49def0d5148310
hanh1012 <email@example.com>
2025-05-12 19:08:30 +0700

    commit muc tuan 1

---
+++ backend/HealthApp/src/main/java/com/vnh/configs/DispatcherServletInit.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.vnh.configs;
+
+/**
+ *
+ * @author hanh7
+ */
+public class DispatcherServletInit {
+    
+}
